2a283418add6a92c993bcda26a4936078d4c6cb1
raposamillar <lisa.raposamillar@gmail.com>
2022-08-26 01:24:00 -0400

    add a for loop to retrieve the five-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 const init = async () => {
-    let city = document.querySelector('input').value;
+  let city = document.querySelector('input').value;
 
-    if (!city) return;
+  if (!city) return;
 
-    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+  let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
 
-    let d = await fetch(url).then(data => data.json());
+  let d = await fetch(url).then(data => data.json());
 
 
 
-document.querySelector('#current-container').innerHTML = `
+  document.querySelector('#current-container').innerHTML = `
     <h2>
       ${city} (${new Date(d.list[0].dt * 1000).toDateString()})
     </h2>
@@ -18,6 +18,22 @@ document.querySelector('#current-container').innerHTML = `
       <p>Temp: ${d.list[0].main.temp}</p>
       <p>Wind: ${d.list[0].wind.speed}</p>
       <p>Humidity: ${d.list[0].main.humidity}</p>`;
+
+  let forecast = document.getElementById('five-day-container');
+
+  for (let i = 7; i < d.list.length; i = i + 7) {
+
+    forecast.innerHTML += `
+    <div class="card">
+      <h3>${new Date(d.list[i].dt * 1000).toDateString()}</h3>
+
+      <img src="http://openweathermap.org/img/wn/${d.list[i].weather[0].icon}@2x.png">
+
+      <p>Temp: ${d.list[i].main.temp}</p>
+      <p>Wind: ${d.list[i].wind.speed} m/h</p>
+      <p>Humidity: ${d.list[i].main.humidity}</p> 
+    </div>`;
+  };
 };
 
 document.querySelector('button').addEventListener('click', init);
